b2d61234148ca64c9e700cc93302a9abd16a88d0
azad <md1azad0@gmail.com>
2023-08-27 22:47:47 +0600

    di

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+// <!-- /////////////////////////////
+// kavascript toggle menu -->
+
+let navLinks = document.getElementById(`navLinks`);
+
+function showmenu() {
+  navLinks.style.left = "0";
+}
+
+function hidemenu() {
+  navLinks.style.left = "-200px";
+}
